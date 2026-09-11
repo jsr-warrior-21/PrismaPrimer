5732e1c33d80f2c98eda7b48822ff147692d7762
Arvind <jsrarvind12@gmail.com>
2026-09-12 01:26:12 +0530

    setting up prisma orm

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
-});
+}); 
