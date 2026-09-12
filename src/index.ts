ae0f1c2c5a429dddeec359b436e096f2034a0ae3
Arvind <jsrarvind12@gmail.com>
2026-09-12 19:50:14 +0530

    added all conditions

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,51 @@ const PORT = process.env.PORT || 3000;
 // getting user
 
 app.get("/user", async (_req: Request, res: Response) => {
-  const response = await prisma.user.findMany();
 
+  // const response = await prisma.user.findMany({where:{isMarried:true}});
+
+
+  //--- gte - greater than equal
+
+  // const response = await prisma.user.findMany({where:{isMarried:true,age:{gte:25}}});
+
+
+  //-- OR conditon (u can add more user like this for or condition)
+
+  // const response = await prisma.user.findMany({where:{
+  //   OR:[
+  //     {nationality:"Indian"},{age:{gte:25}}
+  //   ]
+  // }});
+
+  //-- AND condition
+
+
+  //  const response = await prisma.user.findMany({where:{
+  //   AND:[
+  //     {nationality:"Indian"},{age:{gte:25}}
+  //   ]
+  // }});
+
+
+  //-- negation -- NOT condition
+
+
+  //  const response = await prisma.user.findMany({where:{
+  //   nationality:{not:"Indian"}
+  // }});
+
+
+  //--- IN condition
+
+     const response = await prisma.user.findMany({where:{
+     nationality:{in:["Indian","Australian","British"] //this apply or conditon on each nationality
+}
+  }});
+
+
+
+  
   if (!response) {
     return res.status(404).json({
       message: "User not found",
