9a132406ebee3794949bdb33c8bad5a428ca7c73
Arvind <jsrarvind12@gmail.com>
2026-09-14 00:39:57 +0530

    done with basic CRUD operations

--- src/index.ts
+++ src/index.ts
@@ -10,17 +10,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 3000;
 
-// getting user
+// getting user - (READ OPERATIONS)
 
 app.get("/user", async (_req: Request, res: Response) => {
-
   // const response = await prisma.user.findMany({where:{isMarried:true}});
-
 
   //--- gte - greater than equal
 
   // const response = await prisma.user.findMany({where:{isMarried:true,age:{gte:25}}});
-
 
   //-- OR conditon (u can add more user like this for or condition)
 
@@ -32,42 +29,80 @@ app.get("/user", async (_req: Request, res: Response) => {
 
   //-- AND condition
 
-
   //  const response = await prisma.user.findMany({where:{
   //   AND:[
   //     {nationality:"Indian"},{age:{gte:25}}
   //   ]
   // }});
 
-
   //-- negation -- NOT condition
-
 
   //  const response = await prisma.user.findMany({where:{
   //   nationality:{not:"Indian"}
   // }});
 
-
   //--- IN condition
 
-     const response = await prisma.user.findMany({where:{
-     nationality:{in:["Indian","Australian","British"] //this apply or conditon on each nationality
-}
-  }});
+  const response = await prisma.user.findMany({
+    where: {
+      nationality: {
+        in: ["Indian", "Australian", "British"], //this apply or conditon on each nationality
+      },
+    },
+  });
 
-
-
-  
   if (!response) {
     return res.status(404).json({
       message: "User not found in db",
     });
   }
 
-  return res.status(200).json({
-    body: response,
-  });
+  return res
+    .status(200)
+    .json({
+      success: true,
+      data: response,
+    });
 });
+
+// Updating the user data
+
+app.put("/user", async (_req: Request, res: Response) => {
+  const updatedUser = await prisma.user.update({
+    where: {
+      id: 7,
+    },
+    data: {
+      age: 40,
+      isMarried: true,
+    },
+  });
+  if (!updatedUser) {
+    return res.json("User not found");
+  }
+  return res
+    .status(200)
+    .json({
+      success: true,
+      message: "User updated successfully",
+      data: updatedUser,
+    });
+});
+
+// Deleting the user data
+
+app.delete("/user", async (_req: Request, res: Response) => {
+  const deletedUser = await prisma.user.delete({ where: { id: 14 } });
+  return res.status(204).send();
+});
+app.delete('/user',async(_req:Request,res:Response)=>{
+  const deletedUser = await prisma.user.deleteMany({where:{age:{gte:30}}});
+  return res.status(204).json({success:true,message:"Users deleted successfully."})
+})
+
+
+
+
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
 });
